d42672d029282f9238adde09bc6949791ca925af
Burak Dizlek <dizlekburak@gmail.com>
2019-05-26 20:49:21 +0300

    data binding aktif edildi. recyclerview, cardview, puan gösteriminde yardımcı olacak percentagechartview kütüphaneleri eklendi.

--- app/src/main/java/com/bd/popularmovies/ui/activity/MainActivity.kt
+++ app/src/main/java/com/bd/popularmovies/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bd.popularmovies
+package com.bd.popularmovies.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.bd.popularmovies.R
 
 class MainActivity : AppCompatActivity() {
 
