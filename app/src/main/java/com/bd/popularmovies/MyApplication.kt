741bed6257984fe2730fa2084e6a80bc72c45d2c
Burak Dizlek <dizlekburak@gmail.com>
2019-05-30 14:29:51 +0300

    Stetho kurulumu ok.

--- app/src/main/java/com/bd/popularmovies/MyApplication.kt
+++ app/src/main/java/com/bd/popularmovies/MyApplication.kt
@@ -2,6 +2,7 @@ package com.bd.popularmovies
 
 import android.app.Application
 import com.bd.popularmovies.di.appModule
+import com.facebook.stetho.Stetho
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -13,7 +14,8 @@ class MyApplication : Application() {
             androidContext(this@MyApplication)
             modules(appModule)
         }
-
+        if (BuildConfig.DEBUG)
+            Stetho.initializeWithDefaults(this)
     }
 
 
